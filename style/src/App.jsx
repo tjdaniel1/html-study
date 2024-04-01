f5d3d0a95190cfacc8a49625d5d264a37d7726d9
tjdaniel1 <robot2daniel@gmail.com>
2024-04-01 15:03:23 +0900

    bootstrap

--- style/src/App.jsx
+++ style/src/App.jsx
@@ -2,12 +2,14 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import { Button } from "reactstrap";
 
 function App() {
   return (
     <>
       <div className="box"></div>
       <div className="box"></div>
+      <Button color="danger">danger</Button>
     </>
   );
 }
