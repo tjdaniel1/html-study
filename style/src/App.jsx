755c40ecbb6eec39d95e86af52f2dd79a9377f75
tjdaniel1 <robot2daniel@gmail.com>
2024-04-01 16:02:39 +0900

    emotion

--- style/src/App.jsx
+++ style/src/App.jsx
@@ -2,12 +2,29 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import { css } from "@emotion/css";
 
 function App() {
+  const [color, setColor] = useState("red");
+
   return (
     <>
       <div className="box"></div>
       <div className="box"></div>
+      <input type="color" onChange={(e) => setColor(e.target.value)} />
+      {/*css in js */}
+      <div
+        className={css`
+          width: 100px;
+          height: 100px;
+          background-color: ${color};
+          &:hover {
+            background-color: blue;
+            transition: all 2s;
+            transform: rotate(900deg) scale(2);
+          }
+        `}
+      ></div>
     </>
   );
 }
